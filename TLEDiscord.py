c6a555e840af508a3c36345bfc00a56ab4d887d2
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2023-04-01 13:39:33 -0500

    Modified main bot, several changes

--- TLEDiscord.py
+++ TLEDiscord.py
@@ -1,5 +1,6 @@
 import asyncio
 import cmds
+
 import config
 import datetime
 from datetime import time, timedelta
@@ -16,40 +17,56 @@ tle_prefix = '!'
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=tle_prefix, intents=intents)
 
-bot.add_command(cmds.move)
-bot.add_command(cmds.set_log_channel)
+# bot.add_command(cmds.move)
+# bot.add_command(cmds.set_log_channel)
+# bot.add_command(cmds.toggle_logging)
 
+# Initialize the bot
 @bot.event
 async def on_ready():
     config = util.load_config(bot.guilds[0].id)
     print("----------------------")
-    print("Logged In As")
-    print("Username: %s"%bot.user.name)
-    print("ID: %s"%bot.user.id)
-    print("Log channel:%s"%config['log_channel_name'])
+    print("Logged in as")
+    print("\tUsername: %s"%bot.user.name)
+    print("\tID: %s"%bot.user.id)
+    print("\tLog channel:%s"%config['log_channel_name'])
     print("----------------------")
 
+    print("Bot is running on the following servers:")
+    for guild in bot.guilds:
+        config = util.load_config(guild.id)
+        print(f"\tServer: {guild.name} (ID: {guild.id})")
+        print(f"\t\tMember count: {len(guild.members)}")
+
+    print("----------------------")
+
+    print("Initializing and scheduling tasks...")
+    
     print("Bot Commands:")
     for command in bot.commands:
-        print(f"!{command.name}")
+        print(f"\t!{command.name}")
+    
 
     heartbeat.start()
     daily_report.start()
     check_and_move_users.start()
+    print("Ready...")
 
-@tasks.loop(minutes=15)
+# Loop section
+
+@tasks.loop(minutes=30)
 async def heartbeat():
     for guild in bot.guilds:
         total_users = len(guild.members)
         users_in_voice_chat = sum(1 for member in guild.members if member.voice)
 
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        print(f"{current_time}\t{guild.name}\tTotal Users: {total_users}\tUsers in Voice Chat: {users_in_voice_chat}")
+        print(f"{current_time}\t{guild.name}\tTotal Users: {total_users}\t| Users in Voice Chat: {users_in_voice_chat}")
 
 @tasks.loop(hours=24)
 async def check_and_move_users():
     current_time = datetime.datetime.now(pytz.timezone('America/Chicago'))  # Adjust the timezone if needed
-    target_time = time(hour=11, minute=2)
+    target_time = time(hour=18, minute=00)
 
     if current_time.time() > target_time:
         for guild in bot.guilds:
@@ -68,20 +85,22 @@ async def check_and_move_users():
                 if moved_users_count > 0:
                     print(f'\tMoved {util.pluralize(moved_users_count, "user", "users")} from {twerk_channel.name} to {member_general_channel.name}')
 
+@tasks.loop(hours=24)
+async def daily_report():
+    current_time = datetime.datetime.now(pytz.timezone('America/Chicago'))
+    print(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\tDaily Report: {len(user_joined)} unique users joined a voice channel since yesterday.")
+    user_joined.clear()
+
+# Before loop section
+
 @check_and_move_users.before_loop
 async def before_check_and_move_users():
     now = datetime.datetime.now(pytz.timezone('America/Chicago'))
     target_time = datetime.time(hour=18, minute=00)
     tomorrow = now.date() + timedelta(days=1)
     next_run = datetime.datetime.combine(tomorrow, target_time, tzinfo=now.tzinfo)
-    print(f"Next twerk move run:{(next_run - now).total_seconds()}")
+    #print(f"Next twerk move run:{(next_run - now).total_seconds()}")
     await asyncio.sleep((next_run - now).total_seconds())
-
-@tasks.loop(hours=24)
-async def daily_report():
-    current_time = datetime.datetime.now(pytz.timezone('America/Chicago'))
-    print(f"{current_time.strftime('%Y-%m-%d %H:%M:%S')}\tDaily Report: {len(user_joined)} unique users joined a voice channel since yesterday.")
-    user_joined.clear()
 
 @daily_report.before_loop
 async def before_daily_report():
@@ -89,23 +108,59 @@ async def before_daily_report():
     target_time = datetime.time(hour=6, minute=00)
     tomorrow = now.date() + timedelta(days=1)
     next_run = datetime.datetime.combine(tomorrow, target_time, tzinfo=now.tzinfo)
-    print(f"Next daily report run:{(next_run - now).total_seconds()}")
+    #print(f"Next daily report run:{(next_run - now).total_seconds()}")
     await asyncio.sleep((next_run - now).total_seconds())
 
+# Event section
 
-@bot.before_invoke
-async def log_command(ctx):
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    print(f'{current_time}\t{ctx.guild.name}\t{ctx.author} used {ctx.message.content}')
+async def log_event(guild, log_channel_name, title, description, color, timestamp=None):
+    config = util.load_config(guild.id)
+    if not config.get('logging_enabled', True):
+        return
 
-async def send_embed(log_channel, title, description, color, fields=None, timestamp=None):
-    embed = discord.Embed(title=title, description=description, color=color)
-    if fields:
-        for name, value in fields:
-            embed.add_field(name=name, value=value, inline=False)
-    if timestamp:
-        embed.timestamp = timestamp
+    log_channel = discord.utils.get(guild.text_channels, name=log_channel_name)
+
+    if not log_channel:
+        overwrites = {
+            guild.default_role: discord.PermissionOverwrite(read_messages=False)
+        }
+        log_channel = await guild.create_text_channel(log_channel_name, overwrites=overwrites)
+
+    embed = discord.Embed(title=title, description=description, color=color, timestamp=timestamp)
     await log_channel.send(embed=embed)
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        return
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send(f'Error: Missing required argument: {error.param.name}')
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send(f'Error: Bad argument: {error.param.name}')
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send(f'Error: Missing permissions: {error.missing_perms}')
+    else:
+        await ctx.send(f'Error: {error}')
+
+@bot.event
+async def on_member_remove(member):
+    await log_event(member.guild, config['log_channel_name'], f'{member.display_name} left the server', '', discord.Color.red(), timestamp=datetime.datetime.now())
+
+@bot.event
+async def on_member_update(before, after):
+    if before.nick != after.nick:
+        await log_event(after.guild, config['log_channel_name'], f'{after.display_name} changed their nickname', f'Before: {before.nick}\nAfter: {after.nick}', discord.Color.blue())
+
+@bot.event
+async def on_message_delete(message):
+    await log_event(message.guild, config['log_channel_name'], f'{message.author.display_name} deleted a message', message.content, discord.Color.red(), timestamp=message.created_at)
+
+@bot.event
+async def on_message_edit(before, after):
+    if before.content != after.content:
+        await log_event(after.guild, config['log_channel_name'], f'{after.author.display_name} edited a message', f'Before: {before.content}\nAfter: {after.content}', discord.Color.blue(), timestamp=after.edited_at)
+
 
 @bot.event
 async def on_voice_state_update(member, before, after):
@@ -132,7 +187,7 @@ async def on_voice_state_update(member, before, after):
 #             description = f'> {member.mention} joined `{after.channel.name}`'
 #             color = discord.Color.green()
 #             fields = [(f'Users in {after.channel.name}', util.user_list(after.channel))]
-#             await send_embed(log_channel, title, description, color, fields, now)
+#             await util.send_embed(log_channel, title, description, color, fields, now)
 #         elif after.channel is None:
 #             duration = round((now - user_join_times.pop(user_id, now)).total_seconds())
 #             formatted_duration = util.format_duration(duration)
@@ -143,7 +198,7 @@ async def on_voice_state_update(member, before, after):
 #                 (f'Duration', f'{formatted_duration}'),
 #                 (f'Users in {before.channel.name}', util.user_list(before.channel))
 #             ]
-#             await send_embed(log_channel, title, description, color, fields, now)
+#             await util.send_embed(log_channel, title, description, color, fields, now)
 #         else:
 #             duration = round((now - user_join_times.pop(user_id, now)).total_seconds())
 #             formatted_duration = util.format_duration(duration)
@@ -156,6 +211,6 @@ async def on_voice_state_update(member, before, after):
 #                 (f'Users in {before.channel.name}', util.user_list(before.channel)),
 #                 (f'Users in {after.channel.name}', util.user_list(after.channel))
 #             ]
-#             await send_embed(log_channel, title, description, color, fields, now)
+#             await util.send_embed(log_channel, title, description, color, fields, now)
 
 bot.run(config.TOKEN)
