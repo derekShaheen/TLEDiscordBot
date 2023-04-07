774bc002efb429ed82b1f44d54c10f509707a5ad
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2023-04-06 23:12:01 -0500

    Added loop

--- run_bot.sh
+++ run_bot.sh
@@ -2,10 +2,12 @@
 
 cd /home/derek/TLEDiscordBot
 
-echo "Attempting to update..."
-sudo /usr/bin/git reset --hard
-sudo /usr/bin/git pull
-echo "Starting the bot..."
-python3 TLEDiscord.py
-echo "The bot crashed, restarting in 5 seconds..."
-sleep 5
+while true; do
+  echo "Attempting to update..."
+  sudo /usr/bin/git reset --hard
+  sudo /usr/bin/git pull
+  echo "Starting the bot..."
+  python3 TLEDiscord.py
+  echo "The bot crashed, restarting in 5 seconds..."
+  sleep 5
+done
