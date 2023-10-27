1c1844c1fa62b4e8a991723e275dc78550fe9bf1
Derek Shaheen <38998433+derekShaheen@users.noreply.github.com>
2023-10-27 01:40:10 -0500

    Graph fitting

--- util.py
+++ util.py
@@ -314,7 +314,8 @@ def generate_plot(guilds: list):
     plt.xlabel('Date')
     plt.ylabel('Unique Users')
     plt.title(f'Daily Voice Channel Users')
-    plt.xticks(rotation=45)
+    #plt.xticks(rotation=45)
+    plt.autofmt_xdate()
     plt.legend()
     plt.tight_layout()
 
